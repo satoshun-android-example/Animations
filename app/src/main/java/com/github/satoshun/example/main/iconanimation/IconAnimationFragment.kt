e2e4fdd07537a5d60711061968a43c0e324a0420
SatoShun <shun.sato1@gmail.com>
2019-11-29 01:16:28 +0000

    icon1 2

--- app/src/main/java/com/github/satoshun/example/main/iconanimation/IconAnimationFragment.kt
+++ app/src/main/java/com/github/satoshun/example/main/iconanimation/IconAnimationFragment.kt
@@ -4,11 +4,13 @@ import android.animation.ObjectAnimator
 import android.animation.PropertyValuesHolder
 import android.os.Bundle
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.LinearInterpolator
+import android.view.animation.RotateAnimation
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import com.github.satoshun.example.R
 import com.github.satoshun.example.databinding.IconAnimationFragBinding
-import kotlinx.coroutines.launch
+
 
 class IconAnimationFragment : Fragment(R.layout.icon_animation_frag) {
   private lateinit var binding: IconAnimationFragBinding
@@ -17,17 +19,31 @@ class IconAnimationFragment : Fragment(R.layout.icon_animation_frag) {
     super.onViewCreated(view, savedInstanceState)
     binding = IconAnimationFragBinding.bind(view)
 
+    icon1()
+    icon2()
+  }
+
+  private fun icon1() {
     val rotate = PropertyValuesHolder.ofFloat(
       View.ROTATION,
       -45f,
       45f
     )
-
     ObjectAnimator.ofPropertyValuesHolder(binding.icon1, rotate)
       .setDuration(200L)
       .apply {
         repeatCount = 10
       }
       .start()
+  }
+
+  private fun icon2() {
+    val anim = RotateAnimation(0f, 90f, 15f, 15f)
+    anim.interpolator = LinearInterpolator()
+    anim.repeatCount = Animation.INFINITE
+    anim.repeatMode = Animation.REVERSE
+    anim.duration = 700
+
+    binding.icon2.startAnimation(anim)
   }
 }
